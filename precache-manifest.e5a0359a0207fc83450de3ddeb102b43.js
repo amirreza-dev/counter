0cc8f1cc80a16100b012fc3cf130b3a3e3e47f2f
Amirreza <amirreza.kadz@gmail.com>
2020-10-04 19:13:18 +0330

    Updates

---
+++ precache-manifest.e5a0359a0207fc83450de3ddeb102b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7675c514466c858161eae568a4e4b5c7",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "eecf202b19da68c62fb9",
+    "url": "/counter/static/js/2.206974de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/counter/static/js/2.206974de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6d11cd3379b5a0735f",
+    "url": "/counter/static/js/main.b48f722c.chunk.js"
+  },
+  {
+    "revision": "f0efe914179fd241fe39",
+    "url": "/counter/static/js/runtime-main.60e3ae63.js"
+  }
+]);
